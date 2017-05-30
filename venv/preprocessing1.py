8ce19029acb8f8d13e7db77ee6b059239071bd76
bpang97 <bpang97>
2017-05-30 15:56:08 -0700

    updated p1 with s3 buckets

--- venv/preprocessing1.py
+++ venv/preprocessing1.py
@@ -18,53 +18,63 @@ def invoke_lambda(event, context):
     folder_name = event["folder_name"]
     auth_token = event["auth_token"]
     has_labels = event["has_labels"]
+    if not has_labels:
+        has_labels = ""
     shape_dir = None
     filter_size = 3
     last = False
+
+    conn = boto.connect_s3("AKIAIMQLHJNMP6DOUM4A","8dJAfPZlTjMR1SOcOetImclAmT+G02VkQiuHefdY")
+    b = conn.get_bucket(event['bucket_from'])
+
+    csv_key = b.get_key('trainLabels.csv')
+    csv_key.get_contents_to_filename("/tmp/trainLabels.csv")
+
 
     for content in metadata['contents']: 
         if content['is_dir'] == False:
             paths.append(content['path']) #adds files to paths
             #paths is list of paths to files, has extensions
             
-    if has_labels:
-        c_reader = csv.reader(open('trainLabels.csv', 'r'), delimiter = ',')
-        columns = list(zip(*c_reader))
-        images = columns[0][1:]
-        levels = list(map(int, columns[1][1:]))
-        label_dict = dict(zip(images, levels))
+    
+    c_reader = csv.reader(open('/tmp/trainLabels.csv', 'r'), delimiter = ',')
+    columns = list(zip(*c_reader))
+    images = columns[0][1:]
+    levels = list(map(int, columns[1][1:]))
+    label_dict = dict(zip(images, levels))
 
-        image_list = []
+    image_list = []
 
-        #gets rid of image paths who do not have a label in label_dict
-        for image_path in paths:
-            image_name = image_path.split("/")[-1]
-            actual_name, extension = image_name.split(".")
+    #gets rid of image paths who do not have a label in label_dict
+    for image_path in paths:
+        image_name = image_path.split("/")[-1]
+        actual_name, extension = image_name.split(".")
 
-            if not label_dict.has_key(actual_name):
-                paths.remove(image_paths)
-
-        # total_count = len(paths)
-        # counter = 1
-
-        for key in paths: 
-            # if counter == total_count:
-            #     last = True
-
-            image_path = key
-            image_name = image_path.split("/")[-1]
-            actual_name, extension = image_name.split(".")
-            label = label_dict[actual_name]
-
-            args = {"image_path": image_path, "image_name": actual_name, "label": label, "filter_size": filter_size, "auth_token": event["auth_token"], "is_train": event["is_train"]}
-            invoke_response = lambda_client.invoke(FunctionName="preprocessing2", InvocationType='Event', Payload=json.dumps(args))
-
-            # counter += 1
+        if not label_dict.has_key(actual_name):
+            paths.remove(image_paths)
 
 
-        #invoke the timer function, which sleeps for 4:50mins before invoking preprocessing3
-        args = {"is_train": is_train}
-        invokeTimer = lambda_client.invoke(FunctionName="timer", InvocationType='Event', Payload=json.dumps(args))
+    for key in paths: 
+ 
+
+        image_path = key
+        image_name = image_path.split("/")[-1]
+        actual_name, extension = image_name.split(".")
+        label = label_dict[actual_name]
+
+        if has_labels:
+            args = {"image_path": image_path, "image_name": actual_name, "label": label, "filter_size": filter_size, "auth_token": event["auth_token"], "is_train": event["is_train"], "has_labels": has_labels}
+        else:
+            args = {"image_path": image_path, "image_name": actual_name, "label": None, "filter_size": filter_size, "auth_token": event["auth_token"], "is_train": event["is_train"], "has_labels": has_labels}
+
+
+        invoke_response = lambda_client.invoke(FunctionName="preprocessing2", InvocationType='Event', Payload=json.dumps(args))
+
+
+
+    #invoke the timer function, which sleeps for 4:50mins before invoking preprocessing3
+    args = {"is_train": is_train, "has_labels": has_labels}
+    invokeTimer = lambda_client.invoke(FunctionName="timer", InvocationType='Event', Payload=json.dumps(args))
 
 
 
