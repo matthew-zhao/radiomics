56fbb5d1cb771ef02bbdf5dcd90cbaef1123e6bf
Matthew Zhao <matthew.x.zhao@berkeley.edu>
2017-06-01 14:01:46 -0700

    Use old instances unless they're running, in which case launch new instances

--- venv/trigger_function.py
+++ venv/trigger_function.py
@@ -8,9 +8,21 @@ def trigger_handler(event, context):
     conn = boto.ec2.connect_to_region("us-west-2", aws_access_key_id='AKIAJNKBSOROJ44BVHBQ', \
         aws_secret_access_key='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')
 
-    #images = conn.get_all_images()
-    id = 'i-085e912626ec91bd6'
-    instance = conn.start_instances(id)[0]
+    # First check if any ec2 instances are available (in the stopped mode)
+    # and just use the first one available, if there exists one
+    reservations = conn.get_all_reservations(filters={'instance-state-name': 'stopped'})
+    if len(reservations) > 0:
+        instance_id = reservations[0].instances[0].id
+        instance = conn.start_instances(instance_id)[0]
+    else:
+        #images = conn.get_all_images()
+        #img = images[0]
+        img = 'ami-5cc6a43c'
+        reservation = conn.run_instances(img, instance_type='r4.xlarge', subnet_id='subnet-aab984dd', 
+                                 security_group_ids=['sg-70df8f0b'], key_name='ec2-lambda-compute-node')
+        instance = reservation.instances[0]
+
+    #id = 'i-085e912626ec91bd6'
 
     while instance.update() != "running":
         time.sleep(5)
