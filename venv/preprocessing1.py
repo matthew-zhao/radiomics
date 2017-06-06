defc36a0d42ac4a547ed614a0dfb3d0c4ef73c29
Matthew Zhao <matthew.x.zhao@berkeley.edu>
2017-06-06 15:48:25 -0700

    Added S3 functionality along with dropbox option'

--- venv/preprocessing1.py
+++ venv/preprocessing1.py
@@ -14,16 +14,27 @@ lambda_client = boto3.client('lambda')
 def invoke_lambda(event, context):
     conn = boto.connect_s3("AKIAIMQLHJNMP6DOUM4A","8dJAfPZlTjMR1SOcOetImclAmT+G02VkQiuHefdY")
     b = conn.get_bucket("train-data")
+
+    paths = []
     if event["is_dropbox"]:
         dclient = dropbox.client.DropboxClient(event["auth_token"])
         client = dropbox.Dropbox(event["auth_token"])
         metadata = dclient.metadata(event["folder_name"])
+
+        for content in metadata['contents']: 
+            if content['is_dir'] == False:
+                paths.append(content['path']) #adds files to paths
+                #paths is list of paths to files, has extensions
     else:
         bucket_list = b.list()
 
+        for l in bucket_list:
+            # as long as item in bucket is an image
+            if l.key[-4:] != ".csv":
+                paths.append(l.key)
+
         
     is_train = event["is_train"]
-    paths = []
     folder_name = event["folder_name"]
     auth_token = event["auth_token"]
     has_labels = event["has_labels"]
@@ -31,16 +42,6 @@ def invoke_lambda(event, context):
     shape_dir = None
     filter_size = 3
     last = False
-
-    if not has_labels:
-        has_labels = ""
-
-
-    for content in metadata['contents']: 
-        if content['is_dir'] == False:
-            paths.append(content['path']) #adds files to paths
-            #paths is list of paths to files, has extensions
-
 
     if has_labels:
         csv_key = b.get_key('trainLabels.csv')
@@ -62,21 +63,29 @@ def invoke_lambda(event, context):
 
             if not label_dict.has_key(actual_name):
                 paths.remove(image_paths)
+    else:
+        has_labels = ""
 
 
     image_num = 1
     #this loop always happens
-    for key in paths: 
+    for key in paths:
         image_path = key
         image_name = image_path.split("/")[-1]
         actual_name, extension = image_name.split(".")
-        if has_labels:
+        if has_labels and event["is_train"]:
             label = label_dict[actual_name]
-            args = {"image_path": image_path, "image_name": actual_name, "label": label, "filter_size": filter_size, "image_num": image_num,
-                    "auth_token": event["auth_token"], "is_train": event["is_train"], "has_labels": has_labels, "model_bucket_name": model_bucket_name}
+            args = {"image_path": image_path, "image_name": actual_name, "labels": label, "filter_size": filter_size, "image_num": image_num,
+                    "auth_token": event["auth_token"], "is_train": event["is_train"], "has_labels": has_labels, "model_bucket_name": model_bucket_name,
+                    "bucket_from": "train-data"}
+        elif not has_labels and event["is_train"]:
+            args = {"image_path": image_path, "image_name": actual_name, "labels": None, "filter_size": filter_size, "image_num": image_num,
+                    "auth_token": event["auth_token"], "is_train": event["is_train"], "has_labels": has_labels, "model_bucket_name": model_bucket_name,
+                    "bucket_from": "train-data"}
         else:
-            args = {"image_path": image_path, "image_name": actual_name, "label": None, "filter_size": filter_size, "image_num": image_num,
-                    "auth_token": event["auth_token"], "is_train": event["is_train"], "has_labels": has_labels, "model_bucket_name": model_bucket_name}
+            args = {"image_path": image_path, "image_name": actual_name, "labels": None, "filter_size": filter_size, "image_num": image_num,
+                    "auth_token": event["auth_token"], "is_train": event["is_train"], "has_labels": has_labels, "model_bucket_name": model_bucket_name,
+                    "bucket_from": "testing-image-data"}
 
 
         invoke_response = lambda_client.invoke(FunctionName="preprocessing2", InvocationType='Event', Payload=json.dumps(args))
